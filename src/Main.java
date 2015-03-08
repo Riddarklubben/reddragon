5c0a40d053d0ba188bf57def8908f6189443a9f0
Isak <isak.dl@hotmail.com>
2015-03-08 21:42:49 +0100

    initial commit! :) lets do this

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
+
+/**
+ * Created by isak on 2015-03-08.
+ */
+public class Main {
+
+    private static final String salt = "A long, but constant phrase that will be used each time as the salt.";
+    private static final int iterations = 2000;
+    private static final int keyLength = 256;
+    private static final SecureRandom random = new SecureRandom();
+
+    public static void main(String [] args){
+
+        String passphrase = "The quick brown fox jumped over the lazy brown dog";
+        String plaintext = "Luntarn lets do this! :)";
+        byte [] ciphertext;
+        String recoveredPlaintext = plaintext;
+        try {
+            ciphertext = encrypt(passphrase, plaintext);
+            System.out.println(ciphertext);
+            recoveredPlaintext = decrypt(passphrase, ciphertext);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(recoveredPlaintext);
+    }
+
+
+    private static byte [] encrypt(String passphrase, String plaintext) throws Exception {
+        SecretKey key = generateKey(passphrase);
+
+        Cipher cipher = Cipher.getInstance("AES/CTR/NOPADDING");
+        cipher.init(Cipher.ENCRYPT_MODE, key, generateIV(cipher), random);
+        return cipher.doFinal(plaintext.getBytes());
+    }
+
+    private static String decrypt(String passphrase, byte [] ciphertext) throws Exception {
+        SecretKey key = generateKey(passphrase);
+
+        Cipher cipher = Cipher.getInstance("AES/CTR/NOPADDING");
+        cipher.init(Cipher.DECRYPT_MODE, key, generateIV(cipher), random);
+        return new String(cipher.doFinal(ciphertext));
+    }
+
+    private static SecretKey generateKey(String passphrase) throws Exception {
+        PBEKeySpec keySpec = new PBEKeySpec(passphrase.toCharArray(), salt.getBytes(), iterations, keyLength);
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWITHSHA1AND128BITAES-CBC-BC");
+        return keyFactory.generateSecret(keySpec);
+    }
+
+    private static IvParameterSpec generateIV(Cipher cipher) throws Exception {
+        byte [] ivBytes = new byte[cipher.getBlockSize()];
+        random.nextBytes(ivBytes);
+        return new IvParameterSpec(ivBytes);
+    }
+
+}
